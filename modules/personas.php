c3441f0f9289d6abac776ae7be024fa03f232fb5
Pedro Robles Jopia <problesj@gmail.com>
2025-08-27 03:23:51 +0000

    Corregir referencia incorrecta a header_fixed.php en personas.php

--- modules/personas.php
+++ modules/personas.php
@@ -1,4 +1,4 @@
-<?php include '../includes/header_fixed.php'; ?>
+<?php include '../includes/header.php'; ?>
 
 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
     <h1 class="h2">Gestión de Personas</h1>
